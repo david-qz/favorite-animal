65a04c60e16226f8740153e1cebfaa16b78e7b8e
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-01 15:00:20 -0700

    ToS checkbox enables start button
    'start-button' is a much better name than 'reveal-app-button'. Why
    didn't I think of that first?

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // grab DOM elements
+const tosCheckbox = document.getElementById('tos-checkbox');
+const startButton = document.getElementById('start-button');
 
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
+tosCheckbox.addEventListener('change', () => {
+    startButton.disabled = !tosCheckbox.checked;
+});
