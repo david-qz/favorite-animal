8f780b498cf994f9bf8d900c9ce69d2a170108cb
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-01 15:12:01 -0700

    Select changes dog picture

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const tosCheckbox = document.getElementById('tos-checkbox');
 const startButton = document.getElementById('start-button');
 const dogDesigner = document.getElementById('dog-designer');
+const dogSelect = document.getElementById('dog-select');
+const dogDisplay = document.getElementById('dog-display');
 
 // set event listeners
     // get info from user input
@@ -13,4 +15,9 @@ tosCheckbox.addEventListener('change', () => {
 
 startButton.addEventListener('click', () => {
     dogDesigner.classList.remove('hidden');
+});
+
+dogSelect.addEventListener('change', () => {
+    let selection = `assets/dogs/${dogSelect.value}-dog.jpg`;
+    dogDisplay.src = selection;
 });
