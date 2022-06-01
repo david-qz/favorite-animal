58e48034faae295bf175bf48b3288e9dcacecbd9
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-01 15:05:14 -0700

    Start button shows dog designer
    Taking a second pass over some of these names as I go.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // grab DOM elements
 const tosCheckbox = document.getElementById('tos-checkbox');
 const startButton = document.getElementById('start-button');
+const dogDesigner = document.getElementById('dog-designer');
 
 // set event listeners
     // get info from user input
@@ -8,4 +9,8 @@ const startButton = document.getElementById('start-button');
     // update DOM to reflect the new state
 tosCheckbox.addEventListener('change', () => {
     startButton.disabled = !tosCheckbox.checked;
+});
+
+startButton.addEventListener('click', () => {
+    dogDesigner.classList.remove('hidden');
 });
